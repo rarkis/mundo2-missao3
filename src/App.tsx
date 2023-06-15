a607b5c74a1c622772ddecdc4efa2825277daae1
Alison <202208906893@alunos.estacio.br>
2023-06-15 01:05:52 -0300

    app tests

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,27 @@ const element = LivroLista;
 function App() {
   return (
     <>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div className="container-fluid">
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <a className="nav-link" href="/">Catálogo</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="/livro-dados">Novo</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+
       <h2>Catalogo de Livros</h2>
-      <table>
-        <thead>
+      <table className='table table-hover'>
+        <thead className='table-dark'>
           <th>Título</th>
           <th>Resumo</th>
           <th>Editora</th>
