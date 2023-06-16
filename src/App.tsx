e85b1850310eb836026abbadde2cb14d16f9d64b
Alison <202208906893@alunos.estacio.br>
2023-06-15 21:12:57 -0300

    Nav com react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,80 +1,32 @@
 import React from 'react';
 import './App.css';
 import LivroLista from './LivroLista';
+import { BrowserRouter, Routes, Route, Link} from 'react-router-dom';
+import LivroDados from './LivroDados';
 
 const element = LivroLista;
 function App() {
   return (
     <>
-      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+    <BrowserRouter>
+      <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
         <div className="container-fluid">
-          {/* <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarNav"> */}
             <ul className="navbar-nav">
               <li className="nav-item">
-                <a className="nav-link" href="/">Catálogo</a>
+                <Link className="nav-link" to="/">Catálogo</Link>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="/livro-dados">Novo</a>
+                <Link className="nav-link" to="/dados">Novo</Link>
               </li>
             </ul>
-          {/* </div> */}
         </div>
       </nav>
-          <LivroLista/>
-      {/* <h1 className='text-center'>Catálogo de Livros</h1>
-      <div className='container'>
-
-      <table className='table table-striped'>
-        <thead className='text-bg-dark table-dark'>
-          <th>Título</th>
-          <th>Resumo</th>
-          <th>Editora</th>
-          <th>Autores</th>
-        </thead>
-        <tbody>
-          <tr>
-            <td>
-              <p>Título</p>
-              <button className='btn btn-danger'>Excluir</button>
-
-            </td>
-            <td>Resumo</td>
-            <td>Editora</td>
-            <td>Autores</td>
-          </tr>
-          <tr>
-            <td>
-              <p>Título</p>
-              <button className='btn btn-danger'>Excluir</button>
-
-            </td>
-            <td>Resumo</td>
-            <td>Editora</td>
-            <td>Autores</td>
-          </tr>
-        </tbody>
-      </table>
-      </div> */}
+          <Routes>
+            <Route path="/" element={<LivroLista/>}/>
+            <Route path="/dados" element={<LivroDados/>}/>
+          </Routes>
+    </BrowserRouter>
     </>
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.tsx</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
   );
 }
 
---
+++ src/LivroDados.js
@@ -0,0 +1,20 @@
+export default function LivroDados() {
+  // let [livros, setlivros] = useState([]);
+  // console.log(livros);
+  // let [carregado, setCarregado] = useState(false);
+
+  // useEffect(() => {
+  //   setlivros(controleLivro.obterLivros());
+  //   setCarregado(true);
+  // }, [carregado]);
+
+  // const excluir = (codigo) => {
+  //   controleLivro.excluir(codigo);
+  //   setCarregado(false);
+  // };
+  return (
+    <main>
+      <h1>Olá Mundo</h1>
+    </main>
+  )
+};
